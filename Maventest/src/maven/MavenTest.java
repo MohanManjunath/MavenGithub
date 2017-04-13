8a4ca8f575e9f4d7ed3ec2312eb802eeba2331f9
MOHAN <MOHAN@MOHAN-PC>
2017-04-13 15:18:31 +0530

    Total Project

---
+++ Maventest/src/maven/MavenTest.java
@@ -0,0 +1,14 @@
+package maven;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class MavenTest {
+	
+	@Test
+	public void test()
+	{
+		Reporter.log("Hi",true);
+	}
+
+}
